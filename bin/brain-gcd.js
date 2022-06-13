50885150930e3e944ae38e3efa8bede36321662f
Cristikijian <cristikijian@gmail.com>
2022-06-13 14:59:27 +0300

    games v3.5

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import divisorGame from '../games/brain-divisor.js';
+import divisorGame from '../games/brain-gcd.js';
 import { startGame } from '../src/index.js';
 
 startGame('Find the greatest common divisor of given numbers.', divisorGame);
