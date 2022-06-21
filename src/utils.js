bfdb808a802c7ff68c08ce8ae6b7c69d73a07674
Cristikijian <cristikijian@gmail.com>
2022-06-21 15:08:49 +0300

    games v6.1

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,7 @@
 
 import { MAX_RANDOM_NUMBER, MIN_RANDOM_NUMBER } from './constans.js';
 
+// eslint-disable-next-line import/prefer-default-export
 export function getRandomNumber(min = MIN_RANDOM_NUMBER, max = MAX_RANDOM_NUMBER) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
